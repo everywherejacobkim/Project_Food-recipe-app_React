5c1079a200cf4bb9e57895f94616147adf1d9098
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-09 01:21:52 -0800

    feat: add health-label scroll down for search

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import axios from 'axios';
 import RecipeGallery from './component/recipeGallery';
 function App() {
 
-  const [query, queryEdit] = useState("")
-  const [recipes, recipesEdit] = useState([])
+  const [query, queryEdit] = useState("");
+  const [recipes, recipesEdit] = useState([]);
+  const [healthLabel, healthLabelEdit] = useState("vegan");
 
   const YOUR_APP_ID = '15302604';
   const YOUR_APP_KEY = 'a668c1ac2179cadc1b09acd7c6182b50';
   
-  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
 
   async function getRecipes(){
     let result = await axios.get(url);
     recipesEdit(result.data.hits);
-
+    console.log(result.data);
   }
 
   const onSubmit = (e) => {
@@ -35,7 +36,23 @@ function App() {
               onChange={(e)=>queryEdit(e.target.value)}>
             </input>
             <button>Search</button>
-
+            <select className='healthLabels'>
+              <option onClick={()=> healthLabelEdit("vegan")}>Vegan</option>
+              <option onClick={()=> healthLabelEdit("vegetarian")}>Vegetarian</option>
+              <option onClick={()=> healthLabelEdit("paleo")}>Paleo</option>
+              <option onClick={()=> healthLabelEdit("dairy-free")}>Dairy-free</option>
+              <option onClick={()=> healthLabelEdit("gluten-free")}>Gluten-free</option>
+              <option onClick={()=> healthLabelEdit("wheat-free")}>Wheat-free</option>
+              <option onClick={()=> healthLabelEdit("fat-free")}>Fat-free</option>
+              <option onClick={()=> healthLabelEdit("low-sugar")}>Low-sugar</option>
+              <option onClick={()=> healthLabelEdit("egg-free")}>Egg-free</option>
+              <option onClick={()=> healthLabelEdit("peanut-free")}>Peanut-free</option>
+              <option onClick={()=> healthLabelEdit("tree-nut-free")}>Tree-nut-free</option>
+              <option onClick={()=> healthLabelEdit("soy-free")}>Soy-free</option>
+              <option onClick={()=> healthLabelEdit("fish-free")}>Fish-free</option>
+              <option onClick={()=> healthLabelEdit("shellfish-free")}>Shellfish-free</option>
+            
+            </select>
           </form>
       </div>
 
