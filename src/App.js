0cd00637b256420f18f2b8a2ccf23cf99066dbbb
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-08 21:14:03 -0800

    feat: add input search bar by ingredient

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+
+  const [recipe, recipeEdit] = useState("")
+
   const YOUR_APP_ID = '15302604';
   const YOUR_APP_KEY = 'a668c1ac2179cadc1b09acd7c6182b50';
   
-  let url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  let url = `https://api.edamam.com/search?q=${recipe}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
 
   async function getRecipes(){
     let result = await axios.get(url);
     console.log(result.data);
   }
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
+
   return (
     <div className="App">
-      <h1 onClick={getRecipes}>Search Your Food Recipe</h1>
+      <h1>Search Your Food Recipe</h1>
+      <form className='searchForm' onSubmit={onSubmit}>
+        <input type='text' placeholder='Enter Ingredient' value={recipe} onChange={(e)=>recipeEdit(e.target.value)}></input>
+        <button>Search</button>
+
+      </form>
+
+
+
     </div>
+
+
+
+
   );
 }
 
